625f092c00f7adabcd4d8ccbdb28bf3cd70565ee
dam1 - Búa Alonso <A22AnxoBA@sanclemente.local>
2025-01-20 17:37:46 +0100

    Version 0.1

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function changeOP(){
+    let visibility = document.getElementById("img1");
+    visibility.style.opacity = 100
+}
+function changeOP2(){
+    let visibility = document.getElementById("img1");
+    visibility.style.opacity = 0
+}
+
+
